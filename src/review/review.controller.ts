8018e6d9b79810f78b4808e70cd882bd00c8f024
saroyangor <dr.saroyan98@gmail.com>
2024-06-14 17:10:26 +0300

    base done

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -16,6 +16,7 @@ import { CreateReviewDto } from './dto/create-review.dto';
 import { REVIEW_NOT_FOUND } from './review.constants';
 
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+import { UserEmail } from '../decorators/user-email.decorator';
 
 @Controller('review')
 export class ReviewController {
@@ -37,8 +38,10 @@ export class ReviewController {
 		}
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Get('byProduct/:productId')
-	async get(@Param('productId') productId: string) {
+	async get(@Param('productId') productId: string, @UserEmail() email: string) {
+		// console.log(email);
 		return this.reviewService.findByProductId(productId);
 	}
 }
